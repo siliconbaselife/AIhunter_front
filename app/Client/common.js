c9cc9e935a87720bee31d4d5be089b35e2ce9fc1
李顺东 <lishundong2009@163.com>
2023-12-21 14:54:45 +0800

    初始化结构

---
+++ app/Client/common.js
@@ -0,0 +1,131 @@
+const puppeteer = require('puppeteer');
+const logger = require('../Logger');
+const findChrome = require('../../node_modules/carlo/lib/find_chrome');
+
+class Common {
+    browser;
+
+    newPage = async (options = {width: 1580, height: 900}) => {
+        if (!this.browser) {
+            if (!this.browser) {
+                await this.initBrowser(options);
+            }
+        }
+
+        const page = await this.browser.newPage();
+
+        await this.settingPage(page);
+
+        page.on('error', () => {
+            logger.info("页面出现异常");
+        })
+
+        page.on('close', () => {
+            logger.info('页面被关闭');
+        });
+
+        return page;
+    }
+
+    initBrowser = async (options) => {
+        try {
+            const { width, height} = options;
+            const args = [
+                // '--start-fullscreen',
+                '--disable-notifications=true',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-web-security',
+                '--disable-features=IsolateOrigins,site-per-process',
+                `--window-size=${width},${height}`,
+                `--disable-gpu`,
+                `--disable-dev-shm-usage`
+            ];
+
+            let findChromePath = await findChrome({});
+            let executablePath = findChromePath.executablePath;
+
+            this.browser = await puppeteer.launch({
+                executablePath: executablePath,
+                headless: false,
+                defaultViewport: null,
+                ignoreHTTPSErrors: true,
+                args
+            });
+
+            this.browser.on('disconnected', () => {
+                logger.info('puppeteer disconnected 浏览器异常退出')
+            });
+
+            this.browser.on('targetdestroyed', () => {
+                logger.info('puppeteer targetdestroyed 正常退出');
+                logger.info(`当前还剩 ${this.browser.targets().length} 个page`);
+            })
+        } catch (e) {
+            logger.error(`启动失败##`, e);
+        }
+    }
+
+    settingPage = async (page) => {
+        await page.evaluateOnNewDocument(() => {
+            const newProto = navigator.__proto__;
+            delete newProto.webdriver; //删除 navigator.webdriver字段
+            navigator.__proto__ = newProto;
+      
+            Object.defineProperty(navigator, 'userAgent', {
+              //userAgent在无头模式下有headless字样，所以需覆盖
+              get: () =>
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+            });
+            Object.defineProperty(navigator, 'platform', {
+              get: () => 'MacIntel',
+            });
+            Object.defineProperty(navigator, 'languages', {
+              get: () => ['zh-CN', 'zh', 'en'],
+            });
+            Object.defineProperty(navigator, 'plugins', {
+              get: () => [
+                {
+                  0: {type: "application/x-google-chrome-pdf", suffixes: "pdf", description: "Portable Document Format", enabledPlugin: Plugin},
+                  description: "Portable Document Format",
+                  filename: "internal-pdf-viewer",
+                  length: 1,
+                  name: "Chrome PDF Plugin"
+                },
+                {
+                  0: {type: "application/pdf", suffixes: "pdf", description: "", enabledPlugin: Plugin},
+                  description: "",
+                  filename: "mhjfbmdgcfjbbpaeojofohoefgiehjai",
+                  length: 1,
+                  name: "Chrome PDF Viewer"
+                },
+                {
+                  0: {type: "application/x-nacl", suffixes: "", description: "Native Client Executable", enabledPlugin: Plugin},
+                  1: {type: "application/x-pnacl", suffixes: "", description: "Portable Native Client Executable", enabledPlugin: Plugin},
+                  description: "",
+                  filename: "internal-nacl-plugin",
+                  length: 2,
+                  name: "Native Client"
+                }
+              ]
+            });
+      
+            window.chrome = {
+              app: {},
+              loadTimes: () => {},
+              csi: () => {},
+              runtime: {}
+            }
+        });
+    }
+
+
+    closeWindow = async () => {
+        if (this.browser) {
+          await this.browser.close();
+          this.browser = null;
+        }
+    }
+}
+
+module.exports = Common;
---
+++ app/Client/maimai/client.js
@@ -0,0 +1,31 @@
+const Resume = require('./Resume');
+const Login = require('./Login');
+const Chat = require('./Chat');
+const Common = require('../common');
+const logger = require('../../Logger');
+
+class Client {
+    userInfo;
+
+    constructor() {
+
+    }
+
+    login() {
+
+    }
+
+    bind() {
+
+    }
+
+    getUserInfo() {
+
+    }
+
+    run() {
+
+    }
+}
+
+module.exports = new Client();
