7c1f6b3fb6fcb2ace09891036c5a6c824b791e96
Lau Ho <a731240874@163.com>
2023-12-29 09:29:21 +0800

    启动默认打开后台

--- app/index.js
+++ app/index.js
@@ -1,24 +1,7 @@
-// const http = require('http');
-// const { spawn } = require('child_process');
-
-// const Koa = require("koa");
-// const KoaRouter = require('koa-router');
-// const log4js = require("koa-log4");
-
-
-// const Common = require("./Client/common.js");
-
-// const Client = require("./Client/maimai/client.js");
-// const Logger = require('./Logger');
-
-// global.Logger = Logger;
-
-// let client = new Client();
-// client.run();
-
-
-// console.log("end");
-
+// 启动koa服务
 const Server = require("./Server/index");
-
 new Server();
+
+// 打开后台
+const { BACK_ADMIN_DOMAIN } = require("./Config/index");
+require("child_process").exec(`start ${BACK_ADMIN_DOMAIN}/manage/`);
