85e519bcb49c78cae43e7307aaea8c9e0f82d5a5
Lau Ho <a731240874@163.com>
2023-12-29 17:14:25 +0800

    update

--- app/index.js
+++ app/index.js
@@ -4,4 +4,11 @@ new Server();
 
 // 打开后台
 const { BACK_ADMIN_DOMAIN } = require("./Config/index");
-require("child_process").exec(`start ${BACK_ADMIN_DOMAIN}/manage/`);
+switch (process.platform) {
+    case "darwin": //unix 系统内核
+        require("child_process").exec(`open ${BACK_ADMIN_DOMAIN}/manage/`);
+        break;
+    case "win32": //windows 系统内核
+        require("child_process").exec(`start ${BACK_ADMIN_DOMAIN}/manage/`);
+        break;
+}
