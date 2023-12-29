b11509158ea101c3ba94bac10a9ab92c3bfda7c9
Lau Ho <a731240874@163.com>
2023-12-29 14:43:55 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,18 +1,7 @@
-// // 启动koa服务
-// const Server = require("./Server/index");
-// new Server();
+// 启动koa服务
+const Server = require("./Server/index");
+new Server();
 
-// // 打开后台
-// const { BACK_ADMIN_DOMAIN } = require("./Config/index");
-// require("child_process").exec(`start ${BACK_ADMIN_DOMAIN}/manage/`);
-
-
-// const Common = require("./Client/common.js");
-
-const Client = require("./Client/maimai/client.js");
-const Logger = require('./Logger');
-
-global.Logger = Logger;
-
-let client = new Client();
-client.run();
+// 打开后台
+const { BACK_ADMIN_DOMAIN } = require("./Config/index");
+require("child_process").exec(`start ${BACK_ADMIN_DOMAIN}/manage/`);
