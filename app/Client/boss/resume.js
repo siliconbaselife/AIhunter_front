ea9cbb1727abe93e9d896b24c157f9a69b57ddab
李顺东 <lishundong2009@163.com>
2024-01-01 21:20:34 +0800

    增加一些修改

---
+++ app/Client/boss/resume.js
@@ -0,0 +1,13 @@
+const Base = require('./base');
+const { sleep } = require('../../utils');
+const Request = require('../../utils/Request');
+const logger = require('../../Logger');
+const { BIZ_DOMAIN } = require("../../Config/index");
+
+class Resume extends Base {
+    keywordDelay = 40;
+    peopleCache;
+    getList;
+}
+
+module.exports = Resume;
