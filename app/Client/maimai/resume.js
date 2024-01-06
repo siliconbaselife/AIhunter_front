fe8d1362e4dab4c490271d2705ecda8c0a861ab0
李顺东 <lishundong2009@163.com>
2024-01-06 11:42:58 +0800

    修改

--- app/Client/maimai/resume.js
+++ app/Client/maimai/resume.js
@@ -528,28 +528,28 @@ class Resume extends Base {
     dealSayHiTxt = async(sayMsg, peopleInfo) => {
        let name = peopleInfo.name;
        let textarea = await this.waitElement('//textarea[contains(@class, "templateInput___19bTd")]', this.page);
-    //    let text = await this.page.evaluate(node => node.textContent, textarea);
        let text = await this.page.evaluate(()=> {
            let textarea = document.querySelector(".templateInput___19bTd");
            if (textarea) return textarea.value;
        })
 
        if (sayMsg != text) {
-          logger.info(`脉脉 ${this.userInfo.name} name: ${name} 打招呼需要切换话术 text: ${text}`);
+          logger.info(`脉脉 ${this.userInfo.name} name: ${name} 打招呼需要切换话术 text: ${text} sayMsg: ${sayMsg}`);
           while(text.length > 0) {
             await textarea.focus();
-            // await sleep(200);
-            // await textarea.click({clickCount: 3});
-            // await sleep(500);
-            // await this.page.keyboard.press("Backspace");
-            // await textarea.type("");
             await this.page.evaluate(()=> {
                 let textarea = document.querySelector(".templateInput___19bTd");
                 textarea.value = "";
+                const inputEvent = new Event("input", { bubbles: true });
+                textarea.dispatchEvent(inputEvent);
             })
 
-            text = await this.page.evaluate(node => node.textContent, textarea);
+            text = await this.page.evaluate(()=> {
+                let textarea = document.querySelector(".templateInput___19bTd");
+                if (textarea) return textarea.value;
+            })
             logger.info(`脉脉 ${this.userInfo.name} name: ${name} 打招呼需要切换话术 是否清除干净: ${text}`);
+            await sleep(300);
           }
           await sleep(500);
           await textarea.type(sayMsg);
