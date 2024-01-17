b525974425a897f4e2ee0737eb86b7390d3ec76f
Lau Ho <a731240874@163.com>
2024-01-17 15:14:35 +0800

    猎聘登录

---
+++ app/Client/liepin/base.js
@@ -0,0 +1,28 @@
+const Common = require('../common');
+const logger = require('../../Logger');
+
+class Base extends Common {
+    /** @type {import("puppeteer").Browser;} */
+    browser;
+    /** @type {import("puppeteer").Page;} */
+    page;
+
+    loginUrl = "https://h.liepin.com/account/login";
+
+    constructor(options) {
+        super();
+        logger.info(`liepin base options: ${JSON.stringify(options)}`);
+
+        this.options = options || {};
+        const {page, browser, userInfo} = this.options;
+
+        this.browser = browser;
+        this.page = page;
+
+        if (userInfo)
+            logger.info(`liepin userInfo: ${JSON.stringify(userInfo)}`);
+            this.userInfo = userInfo;
+    }
+}
+
+module.exports = Base;
